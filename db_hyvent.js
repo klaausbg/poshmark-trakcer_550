7c627ef0b7a6f21b619956bb7f519d3f60ebe9a4
Klaus Boeger <klausboeger@hotmail.com>
2025-05-14 14:28:57 -0300

    tables fixed

--- db_hyvent.js
+++ db_hyvent.js
@@ -16,15 +16,16 @@ async function ensureTable() {
 }
 
 async function isSeen(url) {
-  const res = await pool.query("SELECT 1 FROM seen_links WHERE url = $1", [
-    url,
-  ]);
+  const res = await pool.query(
+    "SELECT 1 FROM seen_links_hyvent WHERE url = $1",
+    [url]
+  );
   return res.rowCount > 0;
 }
 
 async function markAsSeen(url) {
   await pool.query(
-    "INSERT INTO seen_links(url) VALUES ($1) ON CONFLICT DO NOTHING",
+    "INSERT INTO seen_links_hyvent(url) VALUES ($1) ON CONFLICT DO NOTHING",
     [url]
   );
 }
